88d3021eb1ea1a7ab9204f42d964bbacbaf677c0
hafizmp <hafiznjh007@gmail.com>
2022-08-27 12:46:08 +0530

    First Commit

---
+++ scripts/music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+    {
+      name: "All Time Low",
+      artist: "Jon Bellion, Stormzy",
+      img: "all time low",
+      src: "All Time Low"
+    },
+    {
+      name: "Ikson Anywhere – Ikson",
+      artist: "Audio Library",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Beauz & Jvna - Crazy",
+      artist: "Beauz & Jvna",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Hardwind - Want Me",
+      artist: "Mike Archangelo",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Jim - Sun Goes Down",
+      artist: "Jim Yosef x Roy",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+    },
+];
